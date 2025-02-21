261ac6806da962136471862c634415a51a5f442e
Kowey987 <iskandersdkv@gmail.com>
2025-02-21 18:34:30 +0300

    ADD: player

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import {renderFunction} from "./shared/lib/renderFunction.js";
 
 renderFunction('./shared/components/sidebar/sidebar.html', 'sidebar').then();
+renderFunction('./widgets/player/player.html', 'player').then();
