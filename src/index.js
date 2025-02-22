f3a9ae92342f74eb26607fdf7ca50c6cfc9b77dd
Kowey987 <iskandersdkv@gmail.com>
2025-02-22 13:42:45 +0300

    ADD: player music volume control

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import {renderFunction} from "./shared/lib/renderFunction.js";
+import {volumeControl} from "./widgets/player/player.js";
 
 renderFunction('./shared/components/sidebar/sidebar.html', 'sidebar').then();
-renderFunction('./widgets/player/player.html', 'player').then();
+renderFunction('./widgets/player/player.html', 'player').then(() => {
+    volumeControl()
+});
+
+
+
---
+++ src/widgets/player/player.js
@@ -0,0 +1,37 @@
+export function volumeControl() {
+  const slider = document.getElementById('rangeSlider');
+  let prevValue = slider.value
+  const volumeIcon = document.getElementById('volume-icon');
+
+  function updateSliderValue() {
+    if (Number(slider.value) === 0) slider.value = prevValue;
+    else slider.value = 0
+    changeIcon()
+  }
+
+  function updateSliderBackground() {
+    if (Number(slider.value) !== 0) {
+      prevValue = slider.value
+    }
+    const value = (slider.value - slider.min) / (slider.max - slider.min) * 100;
+    slider.style.setProperty('--value', `${value}%`);
+    changeIcon()
+  }
+
+  function changeIcon() {
+    if (Number(slider.value) === 0) {
+      volumeIcon.style["fill"] = "#B560FFFF";
+      volumeIcon.innerHTML = `<svg  id="volume-icon_off" viewBox="0 0 24 24" focusable="false" data-testid="VolumeMuteIcon" aria-hidden="true"><path fill-rule="evenodd" d="M7.015 8.007 10.33 4h1.657s.827 3.636.827 8.007c0 4.357-.83 7.993-.83 7.993H10.33l-3.245-3.993H4.667c-.368 0-.667-.325-.667-.727V8.735c0-.402.299-.728.667-.728h2.348Zm1.104 7.16 2.78 3.42c.236-1.366.582-3.837.582-6.58 0-2.76-.346-5.239-.582-6.604L8.043 8.857l-.4.483h-2.31v5.334h2.386l.4.493ZM18 9.53l.943.943L17.414 12l1.529 1.529-.943.943-1.529-1.529-1.528 1.529L14 13.53l1.529-1.529L14 10.473l.943-.943 1.528 1.529L18 9.53Z" clip-rule="evenodd"></path></svg>`;
+    } else {
+      volumeIcon.style["fill"] = "#FFFFFF";
+      volumeIcon.innerHTML =`<svg id="volume-icon_on" viewBox="0 0 24 24" focusable="false" data-testid="VolumeMaxIcon" aria-hidden="true"> <path fill-rule="evenodd" d="M7.015 8.007 10.33 4h1.657s.827 3.636.827 8.007c0 4.357-.83 7.993-.83 7.993H10.33l-3.245-3.993H4.667c-.368 0-.667-.325-.667-.727V8.735c0-.402.299-.728.667-.728h2.348Zm1.104 7.16 2.78 3.42c.236-1.366.582-3.837.582-6.58 0-2.76-.346-5.239-.582-6.604L8.043 8.857l-.4.483h-2.31v5.334h2.386l.4.493Zm9.643-7.786-.943.942c1.137 1.137 1.731 2.399 1.72 3.65-.012 1.267-.646 2.542-1.834 3.686l.925.96c1.451-1.398 2.227-3 2.242-4.634.015-1.617-.715-3.21-2.11-4.604Zm-3.277 3.193c.448.45.683.936.679 1.408-.005.48-.256.973-.727 1.426l.925.96c.735-.707 1.127-1.528 1.135-2.374.008-.838-.362-1.655-1.07-2.362l-.942.942Z" clip-rule="evenodd"></path> </svg>`
+    }
+  }
+
+
+  updateSliderBackground();
+
+  volumeIcon.addEventListener('click', updateSliderValue);
+  slider.addEventListener('input', updateSliderBackground);
+  volumeIcon.addEventListener('click', updateSliderBackground);
+}
