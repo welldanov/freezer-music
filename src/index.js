c06f0eddef20429d30f03d5fcb6c5e4f8b364681
aydar <vildanov.soap@gmail.com>
2025-02-22 14:50:02 +0300

    ADD: sidebar topbar active, albums rendering UPDATE: color schema main

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import {renderFunction} from "./shared/lib/renderFunction.js";
 import {volumeControl} from "./widgets/player/player.js";
+import {genreCollectionRender} from "./widgets/genre-collections/genre-collections.js";
+
 
 renderFunction('./shared/components/sidebar/sidebar.html', 'sidebar').then();
+renderFunction('./shared/components/topbar/topbar.html', 'page-topbar').then();
 renderFunction('./widgets/player/player.html', 'player').then(() => {
     volumeControl()
 });
+genreCollectionRender().then();
+
 
 
 
