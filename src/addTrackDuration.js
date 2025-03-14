db3e7b4743f8cb2530e05f5a3e9fcf6c3442ce0d
aydar <vildanov.soap@gmail.com>
2025-03-14 12:04:53 +0300

    Big music db commit

--- src/addTrackDuration.js
+++ src/addTrackDuration.js
@@ -1,102 +1,52 @@
 const tracks = {
-    "21": {
-        "title": "MUDD",
-        "album": "MUDD",
-        "release_date": "02/09/2022",
-        "cover": "./shared/assets/img/covers/track-covers/$werve, Grioten, Day$okee - MUDD.jpg",
-        "path": "./shared/assets/music/hip-hop/$werve, Grioten, Day$okee - MUDD.mp3",
+    "56": {
+        "title": "Treehouse",
+        "album": "Treehouse",
+        "release_date": "01/01/2016",
+        "cover": "./shared/assets/img/covers/track-covers/Alex G - Treehouse.jpg",
+        "path": "./shared/assets/music/indie/Alex G - Treehouse.mp3",
         "artist_id": [
-            44
+            86
         ]
     },
-    "22": {
-        "title": "Stay With Me",
-        "album": "Stay With Me",
-        "release_date": "11/10/2020",
-        "cover": "./shared/assets/img/covers/track-covers/1nonly - Stay With Me.jpg",
-        "path": "./shared/assets/music/hip-hop/1nonly - Stay With Me.mp3",
+    "57": {
+        "title": "Genesis",
+        "album": "Genesis",
+        "release_date": "15/09/2020",
+        "cover": "./shared/assets/img/covers/track-covers/Grimes - Genesis.jpg",
+        "path": "./shared/assets/music/indie/Grimes - Genesis.mp3",
         "artist_id": [
-            44
+            86
         ]
     },
-    "23": {
-        "title": "Drop It Like It's Hot",
-        "album": "Drop It Like It's Hot",
-        "release_date": "31/05/2020",
-        "cover": "./shared/assets/img/covers/track-covers/HAARPER - Drop It Like It's Hot.jpg",
-        "path": "./shared/assets/music/hip-hop/HAARPER - Drop It Like It's Hot.mp3",
+    "58": {
+        "title": "Vision - Slowed",
+        "album": "Vision",
+        "release_date": "17/12/2019",
+        "cover": "./shared/assets/img/covers/track-covers/Hucci - Vision [Slowed Version].jpg",
+        "path": "./shared/assets/music/indie/Hucci - Vision [Slowed Version].mp3",
         "artist_id": [
-            44
+            86
         ]
     },
-    "24": {
-        "title": "THE ALCHEMIST",
-        "album": "THE ALCHEMIST",
-        "release_date": "30/07/2020",
-        "cover": "./shared/assets/img/covers/track-covers/HAARPER - THE ALCHEMIST.jpg",
-        "path": "./shared/assets/music/hip-hop/HAARPER - THE ALCHEMIST.mp3",
+    "59": {
+        "title": "NOOKS OF PILTOVER",
+        "album": "NOOKS OF PILTOVER",
+        "release_date": "05/10/2024",
+        "cover": "./shared/assets/img/covers/track-covers/SØMATIC - NOOKS OF PILTOVER.jpg",
+        "path": "./shared/assets/music/indie/SØMATIC - NOOKS OF PILTOVER.wav",
         "artist_id": [
-            44
+            86
         ]
     },
-    "25": {
-        "title": "Last Laugh",
-        "album": "Last Laugh",
-        "release_date": "21/04/2024",
-        "cover": "./shared/assets/img/covers/track-covers/iamjakehill - Last Laugh.jpg",
-        "path": "./shared/assets/music/hip-hop/iamjakehill - Last Laugh.mp3",
+    "60": {
+        "title": "Harpy Hare",
+        "album": "Harpy Hare",
+        "release_date": "23/06/2021",
+        "cover": "./shared/assets/img/covers/track-covers/Yaelokre - Harpy Hare.jpg",
+        "path": "./shared/assets/music/indie/Yaelokre - Harpy Hare.mp3",
         "artist_id": [
-            44
-        ]
-    },
-    "26": {
-        "title": "9mm",
-        "album": "Memphis Cult Vol. 6",
-        "release_date": "23/02/2023",
-        "cover": "./shared/assets/img/covers/track-covers/Memphis Cult, Groove Dealers, SPLYXER - 9mm.jpg",
-        "path": "./shared/assets/music/hip-hop/Memphis Cult, Groove Dealers, SPLYXER - 9mm.mp3",
-        "artist_id": [
-            44
-        ]
-    },
-    "27": {
-        "title": "Death Lotto",
-        "album": "Death Lotto",
-        "release_date": "11/07/2022",
-        "cover": "./shared/assets/img/covers/track-covers/ovg!, Grioten - Death Lotto.jpg",
-        "path": "./shared/assets/music/hip-hop/ovg!, Grioten - Death Lotto.mp3",
-        "artist_id": [
-            44
-        ]
-    },
-    "28": {
-        "title": "Rave in the Grave",
-        "album": "Bohemian Psycho",
-        "release_date": "31/03/2019",
-        "cover": "./shared/assets/img/covers/track-covers/REDZED - Rave in the Grave.jpg",
-        "path": "./shared/assets/music/hip-hop/REDZED - Rave in the Grave.mp3",
-        "artist_id": [
-            44
-        ]
-    },
-    "29": {
-        "title": "lick!",
-        "album": "lick!",
-        "release_date": "09/08/2024",
-        "cover": "./shared/assets/img/covers/track-covers/Sadfriendd, Ghostface Playa - lick!.jpg",
-        "path": "./shared/assets/music/hip-hop/Sadfriendd, Ghostface Playa - lick!.mp3",
-        "artist_id": [
-            44
-        ]
-    },
-    "30": {
-        "title": "A HOCKEY MASK AND A REAL BAD ATTITUDE",
-        "album": "A HOCKEY MASK AND A REAL BAD ATTITUDE",
-        "release_date": "07/01/2022",
-        "cover": "./shared/assets/img/covers/track-covers/SXMPRA, Teddy Slugz - A HOCKEY MASK AND A REAL BAD ATTITUDE.jpg",
-        "path": "./shared/assets/music/hip-hop/SXMPRA, Teddy Slugz - A HOCKEY MASK AND A REAL BAD ATTITUDE.mp3",
-        "artist_id": [
-            44
+            86
         ]
     }
 }
